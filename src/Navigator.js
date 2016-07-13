eecc4b3f8959dd6774e03544795af8fbce1dadf9
Mike Smart <mike.smart@jadu.co.uk>
2016-07-13 14:41:54 +0100

    bumping version && getWindowOffset abstracted to method to be more modular when tackling cross browser compatability

--- src/Navigator.js
+++ src/Navigator.js
@@ -7,7 +7,7 @@ import _defaultsDeep from 'lodash.defaultsdeep';
 
 export default class Navigator {
     constructor (options) {
-        Navigator.VERSION = 'v1.1.1';
+        Navigator.VERSION = 'v1.2.0';
 
         const defaults = {
             activeClass: 'active',
